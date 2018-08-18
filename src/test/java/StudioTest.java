01e1efc160240b0fda926a43404341d0b7f03d07
Mark Ditzel <mark.ditzel@ed.ac.uk>
2018-08-18 14:03:38 +0100

    TDD tested Studio class

---
+++ src/test/java/StudioTest.java
@@ -0,0 +1,43 @@
+import models.Gender;
+import models.Owner;
+import models.Studio;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StudioTest {
+// setup empty Class-defined variables
+    Studio studio1;
+    Owner owner1;
+
+
+    @Before
+    public void before(){
+        studio1 = new Studio("NBC", "Universal Pictures", 1912);
+
+        owner1 = new Owner("Bob", 77, Gender.MALE, 55000,true, studio1);
+    }
+
+
+    //This test is not possible as Studio does not store an owner
+//    @Test
+//    public void studioHasOwner(){
+//        assertEquals(0,0);
+//    }
+
+    @Test
+    public void studioHasParentCompany(){
+        assertEquals("NBC", studio1.getParentCompany());
+    }
+
+    @Test
+    public void studioHasStudioName() {
+        assertEquals("Universal Pictures", studio1.getStudioname());
+    }
+
+    @Test
+    public void studioHasDateFounded(){
+        assertEquals(1912, studio1.getDatefounded());
+    }
+}
