d5c17a83270687e3b08d1065887c79b15f38c890
Mark Ditzel <mark.ditzel@ed.ac.uk>
2018-08-18 12:45:23 +0100

    Realised that  havng ENUM for Studio is not a good idea as it cant hold many directors!

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,11 +1,31 @@
 import db.DBHelper;
 import db.HibernateUtil;
+import models.Gender;
+import models.Owner;
+import models.Studio;
 
 import java.util.List;
 
 public class Runner {
 
     public static void main(String[] args) {
+
+        // (1) Create an Owner
+
+        Owner owner1 = new Owner("Bob Carolgees", 67, Gender.MALE, 100000,true, Studio.VIACOM);
+
+        owner1.setAge(68);
+
+        owner1.setStudio(Studio.FOX);
+
+        owner1.setGender(Gender.OTHER);
+
+
+
+
+
+
+
 
 
 
--- src/main/java/models/Owner.java
+++ src/main/java/models/Owner.java
@@ -3,11 +3,21 @@ package models;
 public class Owner extends Person{
 
     //IV
-
     private Studio studio;
 
-    public Owner(String name, int age, Gender gender, double salary, boolean employmentStatus, Studio studio) {
+
+
+    // Constructor
+     public Owner(String name, int age, Gender gender, double salary, boolean employmentStatus, Studio studio) {
         super(name, age, gender, salary, employmentStatus);
+        this.studio = studio;
+    }
+
+    public Studio getStudio() {
+        return studio;
+    }
+
+    public void setStudio(Studio studio) {
         this.studio = studio;
     }
 }
